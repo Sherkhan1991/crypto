2104c37e83f8f42e626f1db1f9d4dcc70df84a67
Arslan <arslanaziz@appsgenii.eu>
2018-07-05 18:29:11 +0500

    UPdate Table
    Create Table Syntax Updated

--- admin/model/extension/payment/coinbase.php
+++ admin/model/extension/payment/coinbase.php
@@ -6,25 +6,30 @@ class ModelExtensionPaymentCoinbase extends Model
     {
         $this->db->query("
 	    	CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "coinbase_order` (
-	        	`coinbase_order_id` INT(11) NOT NULL AUTO_INCREMENT,
-	        	`order_id` INT(11) NOT NULL,
-	        	`coinbase_payment_id` VARCHAR(120),
-	        	`coinbase_secret_key` VARCHAR(100) NOT NULL,
-	        	PRIMARY KEY (`coinbase_order_id`)
+	        	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	        	`store_order_id` INT(11) NOT NULL,
+	        	`store_total_amount` FLOAT NOT NULL,	        	
+	        	`coinbase_charge_code` VARCHAR(50) NOT NULL,
+	        	`coinbase_transaction_id` VARCHAR(100),
+	        	`coinbase_status` TEXT NOT NULL,
+	        	`coinbase_coins_expected` FLOAT NOT NULL,	        	
+	        	`coinbase_coins_received` FLOAT,
+	        	`coinbase_received_currency` TEXT NOT NULL,
+	        	PRIMARY KEY (`id`)
 	     	) ENGINE=MyISAM DEFAULT COLLATE=utf8_general_ci;
     	");
 
         $this->load->model('setting/setting');
 
         $settings = array();
-        $settings['payment_coinbase_api_test_mode'] = 0;
-        $settings['payment_coinbase_order_status_id'] = 1;
-        $settings['payment_coinbase_completed_status_id'] = 2;
-        $settings['payment_coinbase_confirmed_status_id'] = 2;
-        $settings['payment_coinbase_underpayment_status_id'] = 8;
-        $settings['payment_coinbase_invalid_status_id'] = 8;
-        $settings['payment_coinbase_expired_status_id'] = 14;
-        $settings['payment_coinbase_canceled_status_id'] = 7;
+        $settings['coinbase_api_test_mode'] = 0;
+        $settings['coinbase_order_status_id'] = 1;
+        $settings['coinbase_completed_status_id'] = 2;
+        $settings['coinbase_resolved_status_id'] = 5;
+        $settings['coinbase_unresolved_status_id'] = 8;
+        $settings['coinbase_expired_status_id'] = 14;
+        $settings['coinbase_total'] = 30;
+        $settings['coinbase_sort_order'] = 0;
 
         $this->model_setting_setting->editSetting('payment_coinbase', $settings);
     }
